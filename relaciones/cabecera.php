d1328c4b161a9f837f8d3702e67994e9b5101334
dcrugil0102 <dcrugil0102@g.educaand.es>
2025-11-13 10:00:01 +0100

    Auto commit

--- relaciones/cabecera.php
+++ relaciones/cabecera.php
@@ -5,26 +5,6 @@ define("MODO_TRABAJO", "desarrollo"); //en "produccion o en desarrollo
 
 include_once(dirname(__FILE__) . "/scripts/bookstores/validacion.php");
 
-$arrayPuntos = [];
-
-if (file_exists(__DIR__ . $nombrePunto)) {
-    $fic = fopen(__DIR__ . $nombrePunto, "r");
-    while($linea = fgets($fic) !== false){
-        $datos = explode(';', trim($linea));
-            $arrayPuntos[] = new Punto($datos[0],$datos[1], $datos[2], $datos[3]);
-    }
-}
-
-print_r($arrayPuntos);
-
-
-
-if (MODO_TRABAJO == "produccion")
-    error_reporting(0);
-else
-    error_reporting(E_ALL);
-
-
 spl_autoload_register(function ($clase) {
     $ruta = RUTABASE . "/scripts/class/";
     $fichero = $ruta . "$clase.php";
@@ -35,6 +15,33 @@ spl_autoload_register(function ($clase) {
         throw new Exception("La clase $clase no se ha encontrado.");
     }
 });
+
+$arrayPuntos = [];
+$rutaPunto = __DIR__ . "/aplicacion/relacion7/" . $nombrePunto;
+
+if (file_exists($rutaPunto)) {
+    $fic = fopen($rutaPunto, "r");
+    while(($linea = fgets($fic)) !== false){
+        $datos = explode(';', trim($linea));
+            try {
+                $arrayPuntos[] = new Punto($datos[0],$datos[1], $datos[2], $datos[3]);
+            } catch (Exception $err) {
+            $errores[] = $err;
+            }
+    }
+
+    print_r($arrayPuntos);
+} else
+    echo "no existe " . $rutaPunto;
+
+
+
+
+
+if (MODO_TRABAJO == "produccion")
+    error_reporting(0);
+else
+    error_reporting(E_ALL);
 
 include(RUTABASE . "/aplicacion/templates/plantilla.php");
 include(RUTABASE . "/aplicacion/config/acceso_bd.php");
