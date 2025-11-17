f359c8b02e108789b1bca20db31de04b62c48b53
dcrugil0102 <dcrugil0102@g.educaand.es>
2025-11-17 14:44:45 +0100

    Auto commit

--- relacion8/aplicacion/personalizar/personalizar.php
+++ relacion8/aplicacion/personalizar/personalizar.php
@@ -1,2 +1,31 @@
 <?php
+include_once(dirname(__FILE__) . "/cabecera.php");
 
+$barraUbi = [
+    [
+        "TEXTO" => "Inicio",
+        "LINK" => "/index.php"
+    ]
+];
+
+inicioCabecera("2DAW APLICACION");
+cabecera();
+finCabecera();
+
+inicioCuerpo("2DAW APLICACION", $barraUbi);
+cuerpo();
+finCuerpo();
+
+
+
+// **********************************************************
+
+function cabecera() {}
+
+
+function cuerpo()
+{
+?>
+    <p>Estas en el inicio.</p>
+<?php
+}
