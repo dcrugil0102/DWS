4386cb35103d5db7c464a736aafc7d18d861aa15
dcrugil0102 <dcrugil0102@g.educaand.es>
2025-12-09 10:30:02 +0100

    Auto commit

--- examen1/scripts/diciembre/Libro.php
+++ examen1/scripts/diciembre/Libro.php
@@ -9,10 +9,36 @@ class Libro
 
     private $_otras = [];
 
+    // METODOS **************************************
+
+    function añadir() : void {
+
+        $prop = "";
+        $valor = "";
+
+        for ($i = 0; $i < count($this->_otras); $i += 2) {
+            $prop = $this->_otras[$i];
+            $valor = $this->_otras[$i + 1];
+
+            $this->__set($prop, $valor);
+        }
+    }
+
     // PROPIEDADES DINÁMICAS ************************
 
     public function __set($name, $value): void
     {
-        $this->_otras[$name] = $value;
+        $this->$name = $value;
+    }
+
+
+    public function __get($name) : mixed
+    {
+        if (!validaRango($name, $this->_otras, 2)) {
+            return false;
+        }
+        
+        $name = mb_strtolower($name);
+        return $this->$name;
     }
 }
