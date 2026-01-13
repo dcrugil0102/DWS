7550f28010da8d56ea294a769bbe5f70b6fda9c2
dcrugil0102 <dcrugil0102@g.educaand.es>
2026-01-13 10:00:01 +0100

    Auto commit

--- trimestre2/aplicacion/controladores/practicas1Controlador.php
+++ trimestre2/aplicacion/controladores/practicas1Controlador.php
@@ -111,4 +111,13 @@ final class practicas1Controlador extends CControlador
         $this->dibujaVista("ejercicio7", [], "Relacion 1 - Ejercicio 7");
         
     }
+
+    public function accionEjer5(){
+        $vector = array();
+        $vector[1] = "esto es una cadena";
+        $vector["posi1"] = 25.67;
+        $vector[] = false;
+        $vector["ultima"] = array(2, 5, 96);
+        $vector[56] = 23;
+    }
 }
